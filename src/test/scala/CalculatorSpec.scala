69d4c27c7d779d13c09a85bc8af9f03994b3b0ba
Yura Taras <yura.taras@gmail.com>
2013-04-30 11:53:44 +0300

    Adding ??? implementations
    Tests compile but fail

--- src/test/scala/CalculatorSpec.scala
+++ src/test/scala/CalculatorSpec.scala
@@ -7,4 +7,10 @@ class CalculatorSpec extends Specification {
     "mutliply" in { 3 * 4 must_== 12 }
     "divide" in { 10 / 2 must_== 5 }
   }
+  "Calculator" should {
+    "add" in { Calculator.add(3, 3) must_== 6 }
+    "subtract" in { Calculator.subtract(10, 3) must_== 7 }
+    "multiply" in { Calculator.multiply(5, 5) must_== 25 }
+    "divide"   in { Calculator.divide(30, 3) must_== 10 }
+  }
 }
