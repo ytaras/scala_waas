5ae3fcd41d533c35fa051960a52efcd94167faa7
Yura Taras <yura.taras@gmail.com>
2013-05-31 16:09:58 +0300

    Added specs for all validations

--- src/main/scala/validator/validator.scala
+++ src/main/scala/validator/validator.scala
@@ -3,5 +3,8 @@ package validator
 import model._
 
 object WorkflowValidator {
-  def zeroStepsValidation(wf: Workflow): Option[String] = if (wf.steps.isEmpty) Some("Workflow has 0 steps") else None
+  def zeroStepsValidation(wf: Workflow) = if (wf.steps.isEmpty) Some("Workflow has 0 steps") else None
+  def noStartValidator(wf: Workflow): Option[String] = ???
+  def fewStartsValidator(wf: Workflow): Option[String] = ???
+  def orphansValidator(wf: Workflow): Option[String] = ???
 }
--- src/test/scala/validation/validator_spec.scala
+++ src/test/scala/validation/validator_spec.scala
@@ -5,6 +5,7 @@ import org.specs2.ScalaCheck
 import model._
 
 class ValidationSpec extends Specification {
+  import WorkflowValidator._
   val valid = Workflow("wf", List(
     Step("s1", List("s2", "s3"), true),
     Step("s2", List("s3"), false),
@@ -17,12 +18,44 @@ class ValidationSpec extends Specification {
      "pass successfull workflow" in { pending }
   }
   "zero step validator" should {
-     import WorkflowValidator._
      "return None for valid workflow" in {
        zeroStepsValidation(valid) must beNone 
      }
      "return Some for invalid workflow" in {
        zeroStepsValidation(Workflow("wf", Nil)) must beSome
+     }
+  }
+  "no start validator" should {
+     "return None for valid workflow" in {
+       noStartValidator(valid) must beNone 
+     }
+     "return Some for invalid workflow" in {
+       noStartValidator(Workflow("wf", List(
+         Step("s1", Nil, false)
+       ))) must beSome
+     }
+  }
+  "orphan steps validator" should {
+     "return None for valid workflow" in {
+       orphansValidator(valid) must beNone 
+     }
+     "return Some for invalid workflow" in {
+       orphansValidator(Workflow("wf", List(
+         Step("s1", List("s3"), true),
+         Step("s2", List("s3"), false),
+         Step("s3", List("s1"), false)
+       ))) must beSome
+     }
+  }
+  "few start steps validator" should {
+     "return None for valid workflow" in {
+       fewStartsValidator(valid) must beNone 
+     }
+     "return Some for invalid workflow" in {
+       fewStartsValidator(Workflow("wf", List(
+         Step("s1", List("s2"), true),
+         Step("s2", Nil, true)
+       ))) must beSome
      }
   }
 }
