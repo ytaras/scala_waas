5a6745eca4ffdd7256f9bd2c4d0dcfa482b6b221
Yura Taras <yura.taras@gmail.com>
2013-05-31 16:09:58 +0300

    Added specs for all validations

--- src/test/scala/validation/validator_spec.scala
+++ src/test/scala/validation/validator_spec.scala
@@ -5,6 +5,7 @@ import org.specs2.ScalaCheck
 import model._
 
 class ValidationSpec extends Specification {
+  import WorkflowValidator._
   val valid = Workflow("wf", List(
     Step("s1", List("s2", "s3"), true),
     Step("s2", List("s3"), false),
@@ -17,7 +18,35 @@ class ValidationSpec extends Specification {
      "pass successfull workflow" in { pending }
   }
   "zero step validator" should {
-     import WorkflowValidator._
+     "return None for valid workflow" in {
+       zeroStepsValidation(valid) must beNone 
+     }
+     "return Some for invalid workflow" in {
+       zeroStepsValidation(Workflow("wf", Nil)) must beSome
+     }
+  }
+  "no start validator" should {
+     "return None for valid workflow" in {
+       noStartValidator(valid) must beNone 
+     }
+     "return Some for invalid workflow" in {
+       noStartValidator(Workflow("wf", List(
+         Step("s1", Nil, false)
+       ))) must beSome
+     }
+  }
+  "orphan steps validator" should {
+     "return None for valid workflow" in {
+       fewStartsValidator(valid) must beNone 
+     }
+     "return Some for invalid workflow" in {
+       fewStartsValidator(Workflow("wf", List(
+         Step("s1", Nil, true),
+         Step("s2", Nil, false)
+       ))) must beSome
+     }
+  }
+  "zero step validator" should {
      "return None for valid workflow" in {
        zeroStepsValidation(valid) must beNone 
      }
