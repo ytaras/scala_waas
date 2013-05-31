5c3a27dd0f57e15e184b4626353110b8ed2b43f6
Yura Taras <yura.taras@gmail.com>
2013-05-31 19:54:20 +0300

    Solved day2-homework1
    Not the best way, but readable (I hope)

--- src/main/scala/validator/validator.scala
+++ src/main/scala/validator/validator.scala
@@ -3,8 +3,26 @@ package validator
 import model._
 
 object WorkflowValidator {
-  def zeroStepsValidation(wf: Workflow) = if (wf.steps.isEmpty) Some("Workflow has 0 steps") else None
-  def noStartValidator(wf: Workflow): Option[String] = ???
-  def fewStartsValidator(wf: Workflow): Option[String] = ???
-  def orphansValidator(wf: Workflow): Option[String] = ???
+  // These implementations could be shorter, but I tried to make them
+  // easy to read for imperative programmers
+  def zeroStepsValidation(wf: Workflow) = 
+    if (wf.steps.isEmpty) Some("Workflow has 0 steps") else None
+  def noStartValidator(wf: Workflow): Option[String] =
+    if (wf.steps.filter { _.start}.isEmpty)
+      Some("Workflow has 0 start steps") 
+    else None
+  def fewStartsValidator(wf: Workflow): Option[String] =
+    if (wf.steps.filter { _.start }.length > 1 )
+      Some("Workflow has more that 1 start step")
+    else None
+  def orphansValidator(wf: Workflow): Option[String] = {
+    val references = wf.steps.flatMap { _.goesTo }.toSet
+    val notReferenced = wf.steps.filterNot { step =>
+      step.start || (references contains step.name)
+    }.map { _.name }
+    
+    if(notReferenced.isEmpty) None
+    else Some("There are steps that can't be reached")
+  }
+   
 }
